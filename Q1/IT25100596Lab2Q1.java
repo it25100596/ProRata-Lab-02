833f27957033c025156187789538a52771e38c87
it25100596 <it25100596@my.sliit.lk>
2026-09-16 15:13:09 +0530

    Add files via upload

---
+++ Q1/IT25100596Lab2Q1.java
@@ -0,0 +1,13 @@
+public class IT25100596Lab2Q1 {
+
+    public static void main(String[] args) {
+
+        double perimeter = 100;
+
+        double length = perimeter / 3.5;
+        double width = (3.0 / 4.0) * length;
+
+        System.out.println("Length = " + length);
+        System.out.println("Width = " + width);
+    }
+}
