ca008f118edb7a344b4582711591e18f4846f876
it25100596 <it25100596@my.sliit.lk>
2026-09-16 15:14:35 +0530

    IT25100596Lab2Q3

---
+++ Q3/IT25100596Lab2Q3.java
@@ -0,0 +1,12 @@
+public class IT25100596Lab2Q3 {
+
+    public static void main(String[] args) {
+
+        double sideA = 3;
+        double sideB = 4;
+
+        double hypotenuse = Math.sqrt((sideA * sideA) + (sideB * sideB));
+
+        System.out.println("Hypotenuse = " + hypotenuse);
+    }
+}
