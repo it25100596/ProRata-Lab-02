0dcfa90205096082c4e76859d2137547feb26ae3
it25100596 <it25100596@my.sliit.lk>
2026-09-16 15:14:09 +0530

    IT25100596Lab2Q2

---
+++ Q2/IT25100596Lab2Q2.java
@@ -0,0 +1,14 @@
+public class IT25100596Lab2Q2 {
+
+    public static void main(String[] args) {
+
+        double side = 10;
+        double perimeter = 4 * side;
+
+        double PI = 3.14;
+
+        double radius = perimeter / (2 * PI);
+
+        System.out.println("Radius = " + radius);
+    }
+}
